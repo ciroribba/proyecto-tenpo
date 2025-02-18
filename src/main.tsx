3cddec0920887081e45c74ac6e4429c28d8cf1fd
ciroribba <ciro.ribba@gmail.com>
2025-02-18 09:50:09 -0300

    add configprovider ant

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import '@ant-design/v5-patch-for-react-19';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext.tsx';
+import { ConfigProvider } from 'antd';
 import App from './App.tsx';
+import esES from 'antd/es/locale/es_ES';
 import './assets/sass/index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <ConfigProvider locale={esES}>
+        <App />
+      </ConfigProvider>
     </AuthProvider>    
   </StrictMode>,
 )
